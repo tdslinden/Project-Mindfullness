992f520cbdc0067cb807eda6835ed2a3dbe86aa4
tdslinden <linden-k@hotmail.com>
2022-08-31 23:32:50 -0700

    Add useEffect and useState for fetching quotes and fade effect

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/QuoteBox.css";
 import { FaLinkedin, FaTwitter, FaFacebook } from "react-icons/fa";
 
 const QuoteBox = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [quotes, setQuotes] = useState([]);
+  const [fade, setFade] = useState(0);
+
+  const getRandomNumber = () => {
+    const quotesSize = quotes.length;
+    return Math.floor(Math.random() * quotesSize);
+  };
+
+  const getQuote = () => {
+    const randomNumber = getRandomNumber();
+    return quotes[randomNumber];
+  };
+
+  useEffect(() => {
+    async function getQuotes() {
+      try {
+        let response = await fetch(
+          "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+        );
+        let jsonData = await response.json();
+        let retrievedQuotes = [];
+        jsonData["quotes"].forEach((aQuote) => retrievedQuotes.push(aQuote));
+        setQuotes(retrievedQuotes);
+      } catch (err) {
+        alert(err);
+      }
+    }
+    getQuotes();
+  }, []);
+
+  useEffect(() => {
+    let quoteData = getQuote();
+    console.log(quoteData);
+    if (quoteData) {
+      setQuote(quoteData["quote"]);
+      setAuthor(quoteData["author"]);
+    }
+  }, [quotes]);
+
   return (
     <div id="quote-box">
-      <p id="text">
-        "It is only heavy because you are deciding over and over again to carry
-        it. Embrace change, loosen up your sense of identity, let yourself walk
-        a new path. You do not have to ignore or erase the past, you just have
-        to wholeheartedly embrace the present and move on."
+      <p id="text" onAnimationEnd={() => setFade(0)} fade={fade}>
+        "{quote}"
       </p>
 
       <div className="author-container">
-        <p id="author">- Yung Pueblo</p>
+        <p id="author" fade={fade}>
+          - {author}
+        </p>
       </div>
 
       <div className="buttons">
@@ -30,7 +70,16 @@ const QuoteBox = () => {
         </div>
 
         <div>
-          <button className="button" id="new-quote">
+          <button
+            className="button"
+            id="new-quote"
+            onClick={() => {
+              let quoteData = getQuote();
+              setQuote(quoteData["quote"]);
+              setAuthor(quoteData["author"]);
+              setFade(1);
+            }}
+          >
             New Quote
           </button>
         </div>
