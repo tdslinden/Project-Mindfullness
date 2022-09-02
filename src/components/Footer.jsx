2bd10cd205b6316becb068174d0fe83bd71851b9
tdslinden <linden-k@hotmail.com>
2022-09-02 15:20:12 -0700

    Chnage to scss for styling and open new tab when pressing link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import "../styles/Footer.css";
+import "../styles/Footer.scss";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="wrapper">
-        <a id="icon" href="https://www.linkedin.com/in/lindenkyaw/">
+        <a
+          id="icon"
+          href="https://www.linkedin.com/in/lindenkyaw/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaLinkedin />
         </a>
         <p id="tag">By Linden</p>
-        <a id="icon" href="https://github.com/tdslinden">
+        <a
+          id="icon"
+          href="https://github.com/tdslinden"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaGithub />
         </a>
       </div>
